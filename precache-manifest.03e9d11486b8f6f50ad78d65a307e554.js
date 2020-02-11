3e4c74c2a747056e959cf4a9cfe60703903f61bb
quannla <anhquan.nguyenle@gmail.com>
2020-02-12 01:03:00 +0700

    Updates

---
+++ precache-manifest.03e9d11486b8f6f50ad78d65a307e554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3c53f1d171763c3f9e501c3263e678",
+    "url": "/cmc-test/index.html"
+  },
+  {
+    "revision": "24d0700be14817c20e41",
+    "url": "/cmc-test/static/css/main.16470019.chunk.css"
+  },
+  {
+    "revision": "f76d185b64eac57fefe6",
+    "url": "/cmc-test/static/js/2.67b5e32b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cmc-test/static/js/2.67b5e32b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d0700be14817c20e41",
+    "url": "/cmc-test/static/js/main.1b181221.chunk.js"
+  },
+  {
+    "revision": "d10768f19719eaf72138",
+    "url": "/cmc-test/static/js/runtime-main.3e93f323.js"
+  }
+]);
